588d00348ec9509783807f779b673157a12bd949
Ramon Ramirez <39835725+RRamirez1414@users.noreply.github.com>
2020-04-08 16:23:32 -0600

    Update background.js

--- apiExtension/src/background/background.js
+++ apiExtension/src/background/background.js
@@ -26,19 +26,65 @@ chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
             function(tab){
                 //chrome.tabs.executeScript( {file:"../inject/report.js"});  
                 chrome.identity.getAuthToken({'interactive':true}, function (authToken){
-                    //personal YT channel general report
+                    /*
+                    *
+                    * personal YT channel general report
+                    * 
+                    */
+                    // $.ajax({
+                    //     type: 'POST',
+                    //     url:  'https://youtubereporting.googleapis.com/v1/jobs?key=AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
+                    //     headers: {authorization: "Bearer " + authToken},
+                    //     contentType: 'application/json',
+                    //     data: JSON.stringify( { 
+                    //             name: 'POSTRequestTest',
+                    //             reportTypeId: 'channel_subtitles_a2',
+                    //     }),
+                    //     success: function(data){
+                    //         //var responseData = JSON.parse(data);
+                    //         var responseData = JSON.stringify(data);
+                    //         console.log("Success" + responseData);
+                    //     },
+                    //     error: function(response) {
+                    //         console.log("Request Failed");
+                    //         console.log(response.message);
+                    //     }
+                    // }); //end ajax request
+
+
+                    // $.ajax({
+                    //     type: 'GET',
+                    //     /*
+                    //     * In URL from MyFirstJob Report Job
+                    //     * jobId: 9844b0a6-dad4-4655-864a-3d7cd3b7b213
+                    //     * reportId: 4996678818
+                    //     */
+                    //     url:  'https://youtubereporting.googleapis.com/v1/jobs/9844b0a6-dad4-4655-864a-3d7cd3b7b213/reports/4996678818?key=AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
+                    //     headers: {authorization: "Bearer " + authToken},
+                    //     contentType: 'application/json',
+
+                    //     success: function(data){
+                    //         //var responseData = JSON.parse(data);
+                    //         var responseData = JSON.stringify(data);
+                    //         console.log("Success" + responseData);
+                    //     },
+                    //     error: function(response) {
+                    //         console.log("Request Failed");
+                    //         console.log(response.message);
+                    //     }
+                    // }); //end ajax request
+
+
+                    /*
+                    * Retreive the report with URL
+                    *
+                    */
                     $.ajax({
                         type: 'GET',
-                        url:  'https://youtubeanalytics.googleapis.com/v2/reports',
-                        data: {
-                            access_token: authToken,
-                            key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
-                            part: "snippet",
-                            endDate: "2019-06-30",
-                            ids: "channel==MINE",
-                            metrics: "views,comments,likes,dislikes,estimatedMinutesWatched,averageViewDuration",
-                            startDate: "2014-05-01"
-                        },
+                        url:  'https://youtubereporting.googleapis.com/v1/media/CHANNEL/hoWWOlQbCXQR42Xih2AB8g/jobs/9844b0a6-dad4-4655-864a-3d7cd3b7b213/reports/4996678818?alt=media',
+                        headers: {authorization: "Bearer " + authToken },
+                        
+
                         success: function(data){
                             //var responseData = JSON.parse(data);
                             var responseData = JSON.stringify(data);
@@ -50,46 +96,69 @@ chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
                         }
                     }); //end ajax request
                     
-                    //Youtube channel data, interested in playlists UPLOAD ID and CHANNEL ID
-                    $.ajax({
-                        type: 'GET',
-                        url:  'https://www.googleapis.com/youtube/v3/channels',
-                        data: {
-                            access_token: authToken,
-                            key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
-                            part: "snippet, contentDetails, statistics",
-                            mine: "true"
-                        },
-                        success: function(data){
-                            console.log("Success" + data);
-                        },
-                        error: function(e) {
-                            console.log(e.message);
-                        }   
-                    });//end ajax request
+                    // $.ajax({
+                    //     type: 'GET',
+                    //     url:  'https://youtubeanalytics.googleapis.com/v2/reports',
+                    //     data: {
+                    //         access_token: authToken,
+                    //         key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
+                    //         part: "snippet",
+                    //         endDate: "2019-06-30",
+                    //         ids: "channel==MINE",
+                    //         metrics: "views,comments,likes,dislikes,estimatedMinutesWatched,averageViewDuration",
+                    //         startDate: "2014-05-01"
+                    //     },
+                    //     success: function(data){
+                    //         //var responseData = JSON.parse(data);
+                    //         var responseData = JSON.stringify(data);
+                    //         console.log("Success" + responseData);
+                    //     },
+                    //     error: function(response) {
+                    //         console.log("Request Failed");
+                    //         console.log(response.message);
+                    //     }
+                    // }); //end ajax request
+                    
+                    // //Youtube channel data, interested in playlists UPLOAD ID and CHANNEL ID
+                    // $.ajax({
+                    //     type: 'GET',
+                    //     url:  'https://www.googleapis.com/youtube/v3/channels',
+                    //     data: {
+                    //         access_token: authToken,
+                    //         key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
+                    //         part: "snippet, contentDetails, statistics",
+                    //         mine: "true"
+                    //     },
+                    //     success: function(data){
+                    //         console.log("Success" + data);
+                    //     },
+                    //     error: function(e) {
+                    //         console.log(e.message);
+                    //     }   
+                    // });//end ajax request
 
-                    //Youtube playlist ID list, interested in all uploaded videos and video IDs
-                    //Interested in video date first published
-                    $.ajax({
-                        type: 'GET',
-                        url: 'https://www.googleapis.com/youtube/v3/playlistItems',
-                        data: {
-                            access_token: authToken,
-                            key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
-                            part: "snippet, contentDetails",
-                            /*
-                            *   TO DO: Parse through playlists IDs to get UPLOAD ID and pass it here
-                            *          for now, using a static upload playlist ID from personal channel
-                            */
-                            playlistId: "UUhoWWOlQbCXQR42Xih2AB8g" 
-                        },
-                        success: function(data){
-                            console.log("Success" + data);
-                        },
-                        error: function(e) {
-                            console.log(e.message);
-                        }
-                    });//end ajax request
+                    // //Youtube playlist ID list, interested in all uploaded videos and video IDs
+                    // //Interested in video date first published
+                    // $.ajax({
+                    //     type: 'GET',
+                    //     url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+                    //     data: {
+                    //         access_token: authToken,
+                    //         key: 'AIzaSyC_HhJ3YZStGj8e3YJtB9hvFthZ4K4wemE',
+                    //         part: "snippet, contentDetails",
+                    //         /*
+                    //         *   TO DO: Parse through playlists IDs to get UPLOAD ID and pass it here
+                    //         *          for now, using a static upload playlist ID from personal channel
+                    //         */
+                    //         playlistId: "UUhoWWOlQbCXQR42Xih2AB8g" 
+                    //     },
+                    //     success: function(data){
+                    //         console.log("Success" + data);
+                    //     },
+                    //     error: function(e) {
+                    //         console.log(e.message);
+                    //     }
+                    // });//end ajax request
                 });//end getAuthToken function           
             });//end chrome.windows.create
             
