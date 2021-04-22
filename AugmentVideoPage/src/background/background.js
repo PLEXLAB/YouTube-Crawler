a28ecbc662eabbaf03c7d2072a4d929c3385436b
Saleem Masadeh <42525070+saleemMasadeh@users.noreply.github.com>
2021-04-21 21:32:39 -0600

    Video page augmentation extension

---
+++ AugmentVideoPage/src/background/background.js
@@ -0,0 +1,248 @@
+var newTabId 	= -10;
+var windowId 	= -10;
+var vID 		= "" ;
+function callback() {
+    if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+    } else {
+        // Tab exists
+    }
+}
+
+//chrome.browserAction.onClicked.addListener(function(tab){}); for later use
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
+	if(isNaN(response))
+	{
+		// Message sent from inject_video_save script to tell if the DB server is not available 
+		if(response === "NetworkError")
+		{
+			chrome.browserAction.setBadgeText({text: 'Error!'});
+			chrome.windows.get(sender.tab.windowId, function(){
+				if (chrome.runtime.lastError) {
+					console.log(chrome.runtime.lastError.message);
+				} else {
+					chrome.windows.remove(sender.tab.windowId);
+				}
+			});
+		}
+		// Message sent from inject_analytics script
+		if(response === "channelCrawled")
+		{
+			chrome.browserAction.setBadgeText({text: 'Done!'});
+						chrome.windows.get(sender.tab.windowId, function(){
+				if (chrome.runtime.lastError) {
+					console.log(chrome.runtime.lastError.message);
+				} else {
+					chrome.windows.remove(sender.tab.windowId);
+				}
+			});
+		}
+		// Message sent from DB_portal server. "not found" means the consent form has not been filled for the channel to be crawled
+		if(response.chIDFoundStatus === "not found")
+		{
+			chrome.windows.update(sender.tab.windowId, {state:"maximized"},function(windowUpdated){});
+			chrome.tabs.get(newTabId, function(){
+				if (chrome.runtime.lastError) {
+					console.log(chrome.runtime.lastError.message);
+				} else {
+					chrome.tabs.update(newTabId, {url: 'https://plexweb.cs.nmsu.edu/consentForm?chID='+response.chIDString});
+				}
+			});
+		}
+		// Message sent from inject script that crawl data from videos tab
+		if(response === "deleteVtab")
+		{
+			chrome.tabs.getAllInWindow(sender.tab.windowId, function(tabs){chrome.tabs.remove(tabs[0].id);});
+		}
+		// Message sent from inject script after crawling and saving videos Metadata
+		if(response === "get_single_video_Analytics_lifetime")
+		{
+			chrome.storage.sync.get(
+				{list:[]},
+				function(data) {
+					console.log(data.list);
+					vID = data.list.pop();
+					chrome.storage.sync.set(
+						{list : data.list}, 
+						function(){console.log("Video ID is added to videos list");}
+					);
+					if( vID !== undefined){
+						chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/video/' + vID + '/analytics/./period-lifetime'},
+							function(tab){
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_vAnalytics_save.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_Single_video_lifetime.js"});
+						});
+					}
+					else{
+						// Start getting channel analytics after finshing video analytics
+						chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/*/analytics/./period-lifetime'}, 
+							function(tab){
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chAnalytics_save.js"});
+								chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_lifetime.js"});
+						});
+					}
+				}
+			);
+		}
+		//===================not used
+		// Message sent from inject_analytics script that collects data from week range analytics 
+		if(response === "getAnalytics_4week")
+		{
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/*/analytics/./period-4_weeks'}, 
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_4weeks.js"});
+			});
+		}
+		// Message sent from inject_analytics_4weeks script
+		if(response === "getAnalytics_quarter")
+		{
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/*/analytics/./period-quarter'}, 
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_quarter.js"});
+			});
+		}
+		// Message sent from inject_analytics_quarter script
+		if(response === "getAnalytics_year")
+		{
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/*/analytics/./period-year'}, 
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_year.js"});
+			});
+		}
+		// Message sent from inject_analytics_year script
+		if(response === "getAnalytics_lifetime")
+		{
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/*/analytics/./period-lifetime'}, 
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_lifetime.js"});
+			});
+		}
+		//===================end of not used
+		// Message sent from inject/inject_analytics_lifetime
+		if(response.msg === "getAanalytics_explore_chVideo")
+		{
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/' + response.channelID + '/analytics/tab-overview/period-lifetime/explore?entity_type=CHANNEL&entity_id=' + response.channelID + '&time_period=lifetime&explore_type=TABLE_AND_CHART&chart_type=LINE_CHART&metric=VIEWS&granularity=DAY&t_metrics[]=VIDEO_THUMBNAIL_IMPRESSIONS&t_metrics[]=VIDEO_THUMBNAIL_IMPRESSIONS_VTR&t_metrics[]=VIEWS&t_metrics[]=AVERAGE_WATCH_TIME&t_metrics[]=WATCH_TIME&dimension=VIDEO&o_column=VIEWS&o_direction=ANALYTICS_ORDER_DIRECTION_DESC'}, 												 
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chExpAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_explore_video.js"});
+			});
+		}
+		// Message sent from inject_analytics_explore_video
+		if(response.msg === "getAanalytics_explore_Traffic")
+		{						 
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/' + response.channelID + '/analytics/tab-overview/period-lifetime/explore?entity_type=CHANNEL&entity_id=' + response.channelID + '&time_period=lifetime&explore_type=TABLE_AND_CHART&chart_type=LINE_CHART&metric=WATCH_TIME&granularity=DAY&t_metrics[]=WATCH_TIME&t_metrics[]=VIEWS&t_metrics[]=AVERAGE_WATCH_TIME&t_metrics[]=VIDEO_THUMBNAIL_IMPRESSIONS&t_metrics[]=VIDEO_THUMBNAIL_IMPRESSIONS_VTR&dimension=TRAFFIC_SOURCE_TYPE&o_column=WATCH_TIME&o_direction=ANALYTICS_ORDER_DIRECTION_DESC'},	
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chExpAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_explore_Trafic.js"});
+			});
+		}
+		// Message sent from inject_analytics_explore_video
+		if(response.msg === "getAanalytics_explore_Geography")
+		{						 
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/' + response.channelID + '/analytics/tab-overview/period-lifetime/explore?entity_type=CHANNEL&entity_id=' + response.channelID + '&time_period=lifetime&explore_type=TABLE_AND_CHART&chart_type=BAR_CHART&metric=WATCH_TIME&granularity=DAY&t_metrics[]=WATCH_TIME&t_metrics[]=VIEWS&t_metrics[]=AVERAGE_WATCH_TIME&dimension=COUNTRY&o_column=WATCH_TIME&o_direction=ANALYTICS_ORDER_DIRECTION_DESC'},
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chExpAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_explore_Geo.js"});
+			});
+		}
+		// Message sent from inject_analytics_explore_video getAanalytics_explore_Gender
+		if(response.msg === "getAanalytics_explore_Age")
+		{						 
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/' + response.channelID + '/analytics/tab-overview/period-lifetime/explore?entity_type=CHANNEL&entity_id=' + response.channelID + '&time_period=lifetime&explore_type=TABLE_AND_CHART&chart_type=BAR_CHART&metric=WATCH_TIME&granularity=DAY&t_metrics[]=VIEWS&t_metrics[]=AVERAGE_WATCH_TIME&t_metrics[]=AVERAGE_WATCH_PERCENTAGE&t_metrics[]=WATCH_TIME&dimension=VIEWER_AGE&o_column=VIEWER_AGE&o_direction=ANALYTICS_ORDER_DIRECTION_ASC'},
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chExpAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_explore_Age.js"});
+			});
+		}
+		// Message sent from inject_analytics_explore_video get
+		if(response.msg === "getAanalytics_explore_Gender")
+		{						 
+			chrome.tabs.create({windowId: windowId, url: 'https://studio.youtube.com/channel/' + response.channelID + '/analytics/tab-overview/period-lifetime/explore?entity_type=CHANNEL&entity_id=' + response.channelID + '&time_period=lifetime&explore_type=TABLE_AND_CHART&chart_type=BAR_CHART&metric=WATCH_TIME&granularity=DAY&t_metrics[]=VIEWS&t_metrics[]=AVERAGE_WATCH_TIME&t_metrics[]=AVERAGE_WATCH_PERCENTAGE&t_metrics[]=WATCH_TIME&dimension=VIEWER_GENDER&o_column=VIEWER_GENDER&o_direction=ANALYTICS_ORDER_DIRECTION_ASC'},
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/jquery-3.3.1.min.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/overlayScript.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_chExpAnalytics_save.js"});
+					chrome.tabs.executeScript(tab.id, {file: "src/inject/inject_analytics_explore_Gender.js"});
+			});
+		}
+	}
+	else{
+		// Numeric Message containing popup window id of the newly created window sent from popup script
+		windowId = response;
+		chrome.tabs.getAllInWindow(response, function(tabs)
+		{
+			if(tabs.length > 1){
+				newTabId = tabs[1].id;
+				console.log(tabs[1].windowType);
+				console.debug(tabs[1].url);
+			}
+			else{
+				newTabId = tabs[0].id;
+				console.debug(tabs[0].url);
+			}
+		});
+	}
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	// Inject the content script after visiting videos tab 
+	chrome.tabs.get(tab.id,function callback() {
+		if (chrome.runtime.lastError) {
+			console.log(chrome.runtime.lastError.message);
+		} else {
+			if (tab.url.indexOf('https://studio.youtube.com/channel/') != -1 
+				&& tab.url.indexOf('/analytics/') === -1 
+				&& tab.url.indexOf('/videos') === -1)
+			{
+				if(newTabId !== -10)
+				{	
+					chrome.storage.sync.clear();
+					//location.reload();
+					chrome.windows.update(windowId,{state:"normal"},function(windowUpdated){});
+					chrome.tabs.executeScript(newTabId, {file: "src/inject/inject.js"});
+				}
+			}
+			else{
+				// Maximize the popup window to get user attention to login to their YouTube channel
+				if (tab.url.indexOf('https://accounts.google.com/ServiceLogin') != -1) {
+					console.log(tab.url);
+					console.log(windowId);
+					chrome.windows.update(windowId,{state:"maximized"},function(windowUpdated){});
+				}
+				else
+				{
+					if(newTabId !== -10){
+						// Redirect the extension to the main channel page after submitting the consent form successfully 
+						if (tab.url.indexOf('/addConsentForm') != -1) {
+							chrome.tabs.update(newTabId, {url: 'https://studio.youtube.com/channel/'});
+							chrome.windows.update(windowId,{state:"normal"},function(windowUpdated){});
+						}
+					}
+				}
+			}
+		}
+	});
+});
---
+++ AugmentVideoPage/src/inject/inject.js
@@ -0,0 +1,89 @@
+/*
+	This script .....
+	    
+*/
+var vID = window.location.toString()
+vID = vID.split('=').pop();
+
+//===============================================
+// Highlight sentiment bar
+var x = document.querySelector("#movie_player > div.ytp-chrome-bottom");
+
+$('.ytp-gradient-bottom').css('background-color', 'gray');
+$('.ytp-gradient-bottom').css('opacity', '0.5');
+//$('.ytp-gradient-bottom').css('width', 	x.offsetWidth );
+//$('.ytp-gradient-bottom').css('left', 	x.offsetLeft  );
+
+//===============================================
+// Add demontization image element
+var dMonImage 		= document.createElement('img');
+var imgURL 			= chrome.extension.getURL("src/inject/images/demonitizationSymbol.jpg");
+dMonImage.src 		= imgURL;
+var dMonImageDim 	= 25;
+dMonImage.width 	= dMonImageDim;
+dMonImage.height 	= dMonImageDim;
+dMonImage.style.padding = "0px 0px 0px 10px";
+
+$('#container > h1').append($(dMonImage));
+
+//===============================================
+// Add video keywords element
+var keywordsTag = document.createElement('a');
+keywordsTag.innerText = 'Demonetized Keywords: ';
+keywordsTag.style.position 	= 'relative';
+keywordsTag.style.right 	= '-50px';
+keywordsTag.style.color 	= 'white';
+keywordsTag.style.backgroundColor = "red";
+$('#container > h1').append($(keywordsTag));
+
+//===============================================
+// Add number of views per 1st day/latest 7days/and when was the peak so far.
+var viewsTag = document.createElement('a');
+viewsTag.innerText 		= ' | 1st Day: 12345 views | Latest 7 Days: 12345 views | Peak on: Mar 30, 2019';
+viewsTag.style.fontWeight = "900";
+$('#info > #info-text').append($(viewsTag));
+
+//===============================================
+// Create and add a Dropdown list for the recommendation algortihms and it header
+var RecommSection = document.createElement('div');
+var RecommHeader = document.createElement('div');
+RecommHeader.innerText = 'Recommendation Algorithm:';
+RecommHeader.style.color 	= 'white';
+RecommHeader.style.backgroundColor = "red";
+RecommHeader.style.fontSize = "large";
+$(RecommSection).append($(RecommHeader));
+
+var RecommList = document.createElement('div');
+var selectValues = {
+  "1": "YouTube",
+  "2": "Mainstream",
+  "3": "Controversial",
+  "4": "Demonetized",
+  "5": "Positive",
+  "6": "Negative"
+};
+var $mySelect = $('<select></select>');
+$mySelect.width = 400;
+$.each(selectValues, function(key, value) {
+  var $option = $("<option/>", {
+    value: key,
+    text: value
+  });
+  $mySelect.append($option);
+});
+$(RecommList).append($mySelect);
+RecommSection.style.width 	= 400;
+$(RecommSection).append($(RecommList));
+
+$('#items > ytd-compact-autoplay-renderer').append($(RecommSection));
+
+
+//===============================================
+// Add fund source
+var fundSource = document.createElement('a');
+fundSource.innerText = ' -- Self-Funded Commentator';
+fundSource.style.fontWeight = "900";
+fundSource.style.padding = "0px 0px 0px 10px";
+$('#channel-name').append($(fundSource));
+
+
